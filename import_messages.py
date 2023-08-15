a12466a89e908243f5fdaa50a969b5f760e5f521
yuu-eguci <yuu.eguci@gmail.com>
2023-08-15 19:16:00 +0900

    Update import_messages.py

--- import_messages.py
+++ import_messages.py
@@ -10,12 +10,16 @@ def read_latest_file(path: str = "./logs/general") -> list[dict] | None:
         return None
 
     # 一番新しいファイルを見つける
-    files = [f for f in os.listdir(path) if f.endswith('.json')]
-    if not files:
-        print("JSON file not found")
-        return None
+    files = []
+    for f in os.listdir(path):
+        if f.endswith('.json'):
+            try:
+                datetime.strptime(f[:-5], "%Y-%m-%d")
+                files.append(f)
+            except ValueError:
+                continue
 
-    latest_file = max(files, key=lambda x: datetime.strptime(x, "%Y-%m-%d.json"))
+    latest_file = max(files, key=lambda x: datetime.strptime(x[:-5], "%Y-%m-%d"))
     with open(os.path.join(path, latest_file), 'r', encoding='utf-8') as file:
         data = json.load(file)
 
